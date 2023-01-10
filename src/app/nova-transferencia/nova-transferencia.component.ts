424e65005850ae91b814b45d28c583997b0801d9
Leonardo Florêncio <leonardo_florenciop@outlook.com>
2023-01-10 20:04:44 -0300

    Início da função de transferência

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
     selector:'app-nova-transferencia',
@@ -8,12 +8,21 @@ import { Component } from "@angular/core";
 
 export class NovaTransferenciaComponent {
 
+  @Output() aoTransferir = new EventEmitter<any>();
+
     valor: number;
     destino: number;
 
     transferir(){
-      console.log('Solicitada uma nova transferência')
-      console.log('Valor: ', this.valor)
-      console.log('Destino: ', this.destino)
+      console.log('Solicitada uma nova transferência');
+      const valorEmitir = {valor: this.valor, destino: this.destino};
+      this.aoTransferir.emit(valorEmitir);
+      this.limparCampos();
     }
+
+    limparCampos(){
+      this.valor = 0;
+      this.destino = 0;
+    }
+
 }
