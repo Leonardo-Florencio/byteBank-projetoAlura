a31b481f61ac8fcc66b53f47ba65698ce600573b
Leonardo Florêncio <leonardo_florenciop@outlook.com>
2023-01-11 07:16:28 -0300

    Criação inicial do componente extrato

---
+++ src/app/extrato/extrato.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-extrato',
+  templateUrl: './extrato.component.html',
+  styleUrls: ['./extrato.component.scss']
+})
+export class ExtratoComponent {
+
+    @Input() transferencias: any[];
+
+    constructor() {};
+
+    ngOnInit() {};
+}
